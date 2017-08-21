2c6b448fe282f87575bf49047b05f8a6209314d8
Gregory Azevedo <g@lightmatter.com>
2017-08-21 14:39:16 +0200

    inital commit

---
+++ ios-template/ios-template.h
@@ -0,0 +1,19 @@
+//
+//  ios-template.h
+//  ios-template
+//
+//  Created by Gregory Azevedo on 8/21/17.
+//  Copyright © 2017 Lightmatter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios-template.
+FOUNDATION_EXPORT double ios_templateVersionNumber;
+
+//! Project version string for ios-template.
+FOUNDATION_EXPORT const unsigned char ios_templateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_template/PublicHeader.h>
+
+
